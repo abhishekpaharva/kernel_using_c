7fcf77d97ed732c17e5640a89a5a24759fd4305c
Abhishek Paharva <abhishekpaharva12@gmail.com>
2018-09-30 18:13:45 +0530

    Add files via upload

---
+++ types.h
@@ -0,0 +1,17 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+#define true 1
+#define false 0
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long s64;
+
+#endif
