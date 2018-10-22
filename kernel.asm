7e88e65a01968b537b5073f766edf6813c0011bd
Abhishek Paharva <abhishekpaharva12@gmail.com>
2018-10-22 23:18:59 +0530

    added write_port and read_port functions

--- kernel.asm
+++ kernel.asm
@@ -10,7 +10,35 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
+
 extern kmain	        ;kmain is defined in the c file
+extern keyboard_handler_main
+
+read_port:
+	mov edx, [esp + 4]
+			;al is the lower 8 bits of eax
+	in al, dx	;dx is the lower 16 bits of edx
+	ret
+
+write_port:
+	mov   edx, [esp + 4]
+	mov   al, [esp + 4 + 4]
+	out   dx, al
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti 				;turn on interrupts
+	ret
+
+keyboard_handler:
+	call    keyboard_handler_main
+	iretd
 
 start:
   cli 			;block interrupts
